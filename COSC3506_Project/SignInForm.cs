a80e285b4db749b4e60a50324e63d8dcf14a9e4f
Brandon <brandon.jull@gmail.com>
2017-03-10 15:28:26 -0500

    Renaming

---
+++ COSC3506_Project/SignInForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COSC3506_Project
+{
+    public partial class SignInForm : Form
+    {
+        public SignInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: Check for saved username.
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.TextLength > 0)
+            {
+                if (txtPassword.TextLength > 0)
+                {
+                    // TODO: User authentication.
+                    // TODO: Create new User object, initialize object with database data.
+                    // TODO: Check user type, display appropriate form.
+                }
+                else
+                    MessageBox.Show(this, "Please enter a valid password.", "Invalid Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show(this, "Please enter a valid username.", "Invalid Username", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void chkRememberUsername_CheckedChanged(object sender, EventArgs e)
+        {
+            // TODO: Store/remove the username value in configurationn if not already.
+        }
+    }
+}
