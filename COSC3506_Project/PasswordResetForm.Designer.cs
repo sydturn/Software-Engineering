c2ba101ac464efbede30ca9b0b097b3d734877cd
Brandon <brandon.jull@gmail.com>
2017-03-17 14:11:45 -0400

    Cleaned up UI

--- COSC3506_Project/PasswordResetForm.Designer.cs
+++ COSC3506_Project/PasswordResetForm.Designer.cs
@@ -95,7 +95,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 136);
+            this.ClientSize = new System.Drawing.Size(388, 130);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.txtConfirmPassword);
